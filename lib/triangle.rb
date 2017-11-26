cd849e96631fd3cbc4f54efdc590e898b531e6ff
nspp99b <chromepanthers@gmail.com>
2017-11-26 18:31:20 -0500

    solved. not pretty

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+
+  attr_reader :side_one, :side_two, :side_three
+
+  def initialize(side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
+  end
+
+  def kind
+    case
+    when side_one <= 0 || side_two <= 0 || side_three <= 0
+      raise TriangleError
+    when (side_one + side_two <= side_three) || (side_one + side_three <= side_two) || (side_two + side_three <= side_one)
+      raise TriangleError
+    when self.side_one + self.side_two == 2 * self.side_three
+      :equilateral
+    when (self.side_one == self.side_two || self.side_one == self.side_three) && self.side_two != self.side_three
+      :isosceles
+    when self.side_two == self.side_three && self.side_two != self.side_one
+      :isosceles
+    when self.side_one != self.side_two && self.side_two != self.side_three
+      :scalene
+    end
+  end
+end
+
+class TriangleError < StandardError
 end
